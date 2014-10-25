d59760fada5c55f8ae86553e462271128f5e7de0
Moritz <moritz.hilberg@stud.h-da.de>
2014-10-25 13:55:59 +0200

    random mit Monatsabhängigkeit. 1. Versuch eingebaut

--- eclipseProject/src/praktikum/HouseServer.java
+++ eclipseProject/src/praktikum/HouseServer.java
@@ -1,6 +1,10 @@
 package praktikum;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
@@ -29,10 +33,35 @@ public class HouseServer {
 		Random generator = new Random();
 		for (int i = 0; i < getRoomCount(); i++) {
 			Room currentRoom = getRoom(i);
-			currentRoom.setPower(generator.nextInt(150)); // Range 0 <-> 149
-			currentRoom.setTemperature(generator.nextInt(70) - 20); // Range -20
-																	// <-> 49
+			currentRoom.setPower(generator.nextInt(10)); // Range 0 <-> 9
+			double decission = generator.nextDouble();
+			if (decission <= 0.8) {
+				//regular temperature more likely
+				currentRoom.setTemperature(generator.nextInt(16) +10 ); // Range 10 <-> 25
+			}
+			else {
+				//winter low temperature, summer high temperature (extrem high not possible yet)
+				int month = getDate().get(Calendar.MONTH);
+				double factor = (double)(2.0 * 3.1415926535897932 * (month / 11.0)); //MONTH in range  0 to 2*PI
+				double m = -1.0 * Math.cos(factor * 3.1415926535897932 / 180);	//(-cos(MONTH) winter minus possible)
+				int temp = (int)(generator.nextInt(25) * m); //Range 0 <->25 * m
+				currentRoom.setTemperature(temp);
+			}
 		}
+	}
+	
+	private Calendar getDate() {
+		Calendar cal = Calendar.getInstance();
+		//Clear all fields
+	    cal.clear();
+	    //set time
+	    int year = 2014;
+	    int month = 9; //October
+	    int date = 1;
+	    cal.set(Calendar.YEAR, year);
+	    cal.set(Calendar.MONTH, month);
+	    cal.set(Calendar.DATE, date);
+		return cal;
 	}
 
 	public static void main(String[] args) {
