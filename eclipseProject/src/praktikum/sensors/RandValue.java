904f41da81e9f37ed0716decb95a4efb7c7d52c9
Moritz <ocaptainmycaptain@web.de>
2014-11-16 10:58:55 +0100

    Zufallszahlen fuer hohe Temperaturen im Sommer angepasst

--- eclipseProject/src/praktikum/sensors/RandValue.java
+++ eclipseProject/src/praktikum/sensors/RandValue.java
@@ -2,7 +2,7 @@ package praktikum.sensors;
 
 import java.util.Random;
 
-public class RandValue {			//Singleton
+public class RandValue { // Singleton
 	private static RandValue instance = null;
 	Random generator;
 	static boolean instanciated = false;
@@ -27,19 +27,21 @@ public class RandValue {			//Singleton
 				// regular temperature more likely
 				result = generator.nextInt(21) + 10; // Range 10 <-> 30
 			} else {
-				// winter low temperature, summer high temperature (extrem high
-				// not possible yet)
-				double factor = (double) (2.0 * 3.1415926535897932 * (month / 11.0)); // MONTH
-																						// in
-																						// range
-																						// 0
-																						// to
-																						// 2*PI
-				double m = -1.0 * Math.cos(factor * 3.1415926535897932 / 180); // (-cos(MONTH)
-																				// winter
-																				// minus
-																				// possible)
-				result = (int) (generator.nextInt(11) * m); // Range 0 <-> 10 * m
+				// winter low temperature, summer high temperature
+				// MONTH in range 0 to 2*PI
+				double pi = 3.1415926535897932;
+				double factor = (double) (2.0 * pi * (month / 11.0));
+				// -cos(MONTH) (winter minus possible); m [-1,1]
+				double m = -1.0 * Math.cos(factor * pi / 180);
+				if (m > 0) {
+					// Summer april to september
+					// Range (30 <-> 50) * m [0,50]
+					result = (int) ((generator.nextInt(51) - 20) * m);
+				} else {
+					// winter october to march
+					// Range 0 <-> 10 * m [-10,10]
+					result = (int) (generator.nextInt(11) * m);
+				}
 			}
 		}
 		return result;
